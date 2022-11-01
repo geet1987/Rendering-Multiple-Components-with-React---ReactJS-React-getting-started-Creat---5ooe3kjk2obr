d25cfee70c3897f929d89d74d66bcf785e605998
geet1987 <52599381+geet1987@users.noreply.github.com>
2022-11-01 10:09:23 +0530

    Update App.js
    did project name and description

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,25 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
 
-const App = () => {
+const project = ({name,description})=>{
+  return(
+    <>
+    <p data-ns-test="project-name">{name}</p>{" "}
+<p data-ns-test="project-description">{description}</p>
+</>
+);
+};
+function App (){
   return (
-    <div id="main"></div>
-  )
+    <>
+    <project
+    name="E-commerce"
+    description="Build an end-to-end Ecommerce App"
+    />
+    <project name="Netflix clone" description="Built a nice Netflix clone">
+    <project name="Game" description="Create a 2D Javascript Game"/>
+    </>
+  );
 }
 
 
